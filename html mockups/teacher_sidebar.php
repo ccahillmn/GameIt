6580fcdef39def6ed0d518beac036c1f7087690c
jacobaseverson <jacoba.severson@gmail.com>
2014-05-04 11:00:02 -0500

    forgot to actually add the teacher sidebar file

---
+++ html mockups/teacher_sidebar.php
@@ -0,0 +1,16 @@
+<aside class="col-sm-3 col-md-2 sidebar">
+                <h1>Hi, Beatrice.</h1>
+                <img src="../images/girl.png" id="profile_photo" class="img-thumbnail">
+                <h2>Manage</h2>
+                <ul>
+                    <li><a href="map.php" class="out">Classes<span class="glyphicon glyphicon-chevron-right"></span></a></li>
+                    <li><a href="map.php" class="out">Students<span class="glyphicon glyphicon-chevron-right"></span></a></li>
+                    <li><a href="map.php" class="out">Quests<span class="glyphicon glyphicon-chevron-right"></span></a></li>
+                    <li><a href="map.php" class="out">Tasks<span class="glyphicon glyphicon-chevron-right"></span></a></li>
+                </ul>
+                <h2>Your Classes</h2>
+                <ul>
+                        <li><a href="map.php" class="out">Environmental Science <span class="glyphicon glyphicon-chevron-right"></span></a></li>
+                        <li><a href="map.php" class="out">Geometry <span class="glyphicon glyphicon-chevron-right"></span></a></li>
+                </ul>
+    </aside>
