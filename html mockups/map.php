fc2394a3f121c870b3224cc9b6deeb069d2c7e8e
Caitlin Cahill <caitlin.cahill@gmail.com>
2014-05-04 08:44:05 -0500

    Added map page

--- html mockups/map.php
+++ html mockups/map.php
@@ -6,7 +6,9 @@
 	<?php include 'sidebar.php'; ?>
 
 	<!--Map-->
-	<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+	<div id="map" class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+		<h1>Environmental Science</h1>
+		<img class="col-md-12" src="../images/map.png" title="Ennvironmental Science Curriculum Map">
 	</div>
 
 <?php include 'footer.php'; ?>
